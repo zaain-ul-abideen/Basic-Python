2f6a542b2c65264de111ae36789a4ffd20bccbc1
zaain-ul-abideen <mzainkhan40@gmail.com>
2024-11-07 10:24:25 +0500

    Basic Of Python

---
+++ my.basic.code.py
@@ -0,0 +1,40 @@
+                            # CHAPTER 1
+#  DATA TYPE  
+
+a = 1 # a is a integer
+b = 5.66 # b is a flaoting point number 
+c = "harry" # c is string 
+d = False # d is a boolean variable 
+e = None # e is a none type 
+
+# PYTHON IDENTIFICATION THE TYPE OF DATA 
+
+a = 71      # identifies a as class <int>
+b = 88.22   # identifies a as class <float>
+c = "harry" # identifies a as class <str>
+
+#  RULES FOR DEFINING A VARIABLES NAMES 
+
+same_r = "sameer2"   # (variables name can cantains only alphabets, digits and underscores)
+_sameer = "Sameer"   # (variables name can start only alphabets and underscores) 
+# 5smaeer            # (variables name can't start with digits)
+  
+  # ARITHMETIC OPERATORS
+
+a = 5
+b = 6
+c = (a+b)  # Addition  
+c = (a-b)  # Subtraction 
+c = (a*b)  # Multiplication 
+c = (a/b)  # Division 
+c = (a//b) # Double Division (How it works) ##remove the value after the decimal
+print("Addition",a+b, "Subtraction",a-b,"Multiplication",a*b,"Division",a/b,"Double Division",a//b)
+
+
+  # STRING SLICING 
+
+name = "harry"
+
+nameshort = name[ 0:4  ]  
+print(name)
+      
